1f5cc2e6d17cb8d18f6b29e668aa7378e77884e3
Rafael Bayer <rafael.bayer@tegis.com.br>
2024-07-19 11:58:02 -0300

    Adicionado icones e novas cores ao header e footer

--- src/screens/template/Footer.jsx
+++ src/screens/template/Footer.jsx
@@ -1,20 +1,27 @@
 import React from "react";
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 
 const Footer = () => {
   return (
-    <footer class="text-white py-8">
-      <div class="text-center">
-        <p class="mb-4">© 2024 Meu Nome. Todos os direitos reservados.</p>
-        <div class="flex justify-center space-x-4">
-          <a href="https://github.com" class="hover:text-blue-500">
+    <footer className="text-white py-8">
+      <div className="text-center">
+        <p className="mb-4">© 2024 Rafael Bayer. Todos os direitos reservados.</p>
+        <div className="flex justify-center gap-x-4">
+          <a href="https://github.com/rafaelBayer"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="Clique para acessar o Github" className="hover:text-blue-500 inline-flex items-center gap-x-1">
+          <AiFillGithub size="1rem" />
             GitHub
           </a>
-          <a href="https://linkedin.com" class="hover:text-blue-500">
+          <a href="https://www.linkedin.com/in/rafaelbayer0/"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="Clique para acessar o Linkedin" className="hover:text-blue-500 inline-flex items-center gap-x-1">
+              <AiFillLinkedin size="1rem" />
             LinkedIn
           </a>
-          <a href="https://twitter.com" class="hover:text-blue-500">
-            Twitter
-          </a>
+         
         </div>
       </div>
     </footer>
--- src/screens/template/Header.jsx
+++ src/screens/template/Header.jsx
@@ -1,67 +1,62 @@
-import React, { useState } from "react";
-import { AiOutlineHome } from "react-icons/ai";
+import React from "react";
+import { AiFillGithub, AiFillLinkedin, AiOutlineFundProjectionScreen, AiOutlineHome, AiOutlineUser } from "react-icons/ai";
 
 const Header = () => {
-  const [hover, setHover] = useState(false);
-
-  console.log(hover);
   return (
     <header className="flex text-white p-4">
-      <div className="w-1/2">
-        <h1 className="text-2xl font-bold">RB.</h1>
+      <div className="w-1/3">
+        <h1 className="text-3xl font-bold">Rb.</h1>
       </div>
-      <div className="flex justify-end w-1/2">
-        <div className="" onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
-          <a href="#home" class="hover:text-blue-400 inline-flex items-center">
-          <AiOutlineHome/>
+      <div className="flex justify-center w-2/3 gap-x-16 text-lg">
+        <div className="group">
+          <a href="#home" class="group-hover:text-blue-500 inline-flex items-center gap-x-2">
+            <AiOutlineHome />
             Home
           </a>
-          <div className={`bottom-0 left-0 h-1 bg-blue-400 transition-all duration-300 ${hover ? "w-full" : "w-0"}`}></div>
+          <div className="w-0 bottom-0 left-0 h-1 bg-blue-500 transition-all duration-300 rounded-lg group-hover:w-full"></div>
         </div>
-        <div>2</div>
+
+        <div className="group">
+          <a href="#sobre" class="group-hover:text-blue-500 inline-flex items-center gap-x-2">
+            <AiOutlineUser />
+            Sobre
+          </a>
+          <div className={`w-0 bottom-0 left-0 h-1 bg-blue-500 transition-all duration-300 rounded-lg group-hover:w-full`}></div>
+        </div>
+
+        <div className="group">
+          <a href="#projetos" class="group-hover:text-blue-500 inline-flex items-center gap-x-2">
+            <AiOutlineFundProjectionScreen />
+            Projetos
+          </a>
+          <div className={`w-0 bottom-0 left-0 h-1 bg-blue-500 transition-all duration-300 rounded-lg group-hover:w-full`}></div>
+        </div>
+
+        <div className="inline-flex items-center gap-x-2">
+          <a
+            className="hover:text-blue-500"
+            href="https://github.com/rafaelBayer"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="Clique para acessar o Github"
+          >
+            <AiFillGithub size="1.8rem" />
+          </a>
+
+          <a
+            className=" hover:text-blue-500"
+            href="https://www.linkedin.com/in/rafaelbayer0/"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="Clique para acessar o Linkedin"
+          >
+            <AiFillLinkedin size="1.8rem" />
+          </a>
+
+          
+        </div>
       </div>
     </header>
-    // <header class="text-white p-4">
-    //   <div class=" flex justify-between items-center">
-    //     <h1 class="text-2xl font-bold">Rafael Bayer</h1>
-    //     <nav>
-    //       <ul class="flex space-x-6">
-    //         <li className="w-full" onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
-    //           <a href="#home" class="hover:text-blue-400">
-    //             <div className="row">
-    //             <div className="col-auto">
-    //             <AiOutlineHome/>
-    //             </div>
-    //             <div className="col-auto">
-
-    //             Home
-    //             </div>
-
-    //             </div>
-    //           </a>
-    //           <div
-    //             className={`bottom-0 left-0 h-1 bg-blue-400 transition-all duration-300 ${hover ? "w-full" : "w-0"}`}
-    //           ></div>
-    //         </li>
-    //         <li>
-    //           <a href="#about" class="hover:text-blue-400">
-    //             Sobre
-    //           </a>
-    //         </li>
-    //         <li>
-    //           <a href="#projects" class="hover:text-blue-400">
-    //             Projetos
-    //           </a>
-    //         </li>
-    //         <li>
-    //           <a href="#contact" class="hover:text-blue-400">
-    //             Contato
-    //           </a>
-    //         </li>
-    //       </ul>
-    //     </nav>
-    //   </div>
-    // </header>
   );
 };
 
