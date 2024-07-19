37555cb7320c26718fcce9dc26cb24e7af099bfa
Rafael Bayer <rafael.bayer@tegis.com.br>
2024-07-19 15:30:46 -0300

    Adicionado novos icones ao cabeçalho

--- src/screens/template/Header.jsx
+++ src/screens/template/Header.jsx
@@ -1,19 +1,19 @@
 import React, { useState } from "react";
 import { AiFillGithub, AiFillLinkedin, AiOutlineFundProjectionScreen, AiOutlineHome, AiOutlineUser } from "react-icons/ai";
+import { FaReact } from "react-icons/fa";
 import { HiMenuAlt3 } from "react-icons/hi";
-import { IoCloseSharp } from "react-icons/io5";
-import { VscChromeClose } from "react-icons/vsc";
+import { IoClose } from "react-icons/io5";
 
 const Header = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
 
   function toggleMenu() {
-    setIsOpen(!isOpen);
+    setShowMenu(!showMenu);
   }
   return (
     <header className="flex text-white p-4">
       <div className="w-1/3">
-        <h1 className="text-3xl font-bold">Rb.</h1>
+        <h1 className="text-3xl font-bold inline-flex gap-x-2"> <FaReact color="#61DAFB" />Rb.</h1>
       </div>
       {/* DESKTOP */}
       <div className="mob:hidden flex justify-center w-2/3 tablet:gap-x-16 text-lg">
@@ -68,28 +68,10 @@ const Header = () => {
       {/* MOBILE */}
       <div class="tablet:hidden w-full flex justify-end">
         <button onClick={() => toggleMenu()} className="text-white focus:outline-none">
-          {isOpen ? (
-            <>
-              <VscChromeClose size="2rem"/>
-            </>
-          ) : (
-            <>
-          {/* <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
-          </svg> */}
-          <HiMenuAlt3 size="2rem"/>
-            
-            </>
-          )}
+          {showMenu ? <IoClose size="2rem" /> : <HiMenuAlt3 size="2rem" />}
         </button>
 
-        <div className={`w-auto flex items-center ${isOpen ? "block" : "hidden"}`}>
-          {/* <ul className="text-white flex-row">
-            <li><a href="#home" className="block px-2 py-1">Home</a></li>
-            <li><a href="#about" className="block px-2 py-1">Sobre</a></li>
-            <li><a href="#services" className="block px-2 py-1">Projetos</a></li>
-            <li><a href="#contact" className="block px-2 py-1">Contact</a></li>
-          </ul> */}
+        <div className={`w-auto flex items-center ${showMenu ? "block" : "hidden"}`}>
           <div className="flex-row">
             <div className="block px-2 py-1">
               <a href="#home" class="inline-flex items-center">
