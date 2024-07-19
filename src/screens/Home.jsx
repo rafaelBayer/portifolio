8d40355fcbf66807c44e49e225e0722607de9302
Rafael Bayer <rafael.bayer@tegis.com.br>
2024-07-19 17:02:42 -0300

    Adicionado imagem da home

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
+
 import Template from './template/Template'
+import Imagem from '../assets/codeImage.png'
 
 const Home = () => {
     const page = "Home";
   return (
     <Template page={page}>
-       <div>a</div>
+      <div className="flex flex-col">
+        <div className="w-full text-left">
+          
+        </div>
+      </div>
+       
 
+       {/* <img src={Imagem} alt="a" /> */}
     </Template>
   )
 }
