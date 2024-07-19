4e5927aae6c4d5da19202dee7fb5aa3214ce5fa3
Rafael Bayer <rafael.bayer@tegis.com.br>
2024-07-19 15:14:34 -0300

    Ajustado responsividade para mobile

--- src/screens/template/Header.jsx
+++ src/screens/template/Header.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiFillGithub, AiFillLinkedin, AiOutlineFundProjectionScreen, AiOutlineHome, AiOutlineUser } from "react-icons/ai";
+import { HiMenuAlt3 } from "react-icons/hi";
+import { IoCloseSharp } from "react-icons/io5";
+import { VscChromeClose } from "react-icons/vsc";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function toggleMenu() {
+    setIsOpen(!isOpen);
+  }
   return (
     <header className="flex text-white p-4">
       <div className="w-1/3">
         <h1 className="text-3xl font-bold">Rb.</h1>
       </div>
-      <div className="flex justify-center w-2/3 gap-x-16 text-lg">
+      {/* DESKTOP */}
+      <div className="mob:hidden flex justify-center w-2/3 tablet:gap-x-16 text-lg">
         <div className="group">
           <a href="#home" class="group-hover:text-blue-500 inline-flex items-center gap-x-2">
             <AiOutlineHome />
@@ -52,10 +61,84 @@ const Header = () => {
           >
             <AiFillLinkedin size="1.8rem" />
           </a>
-
-          
         </div>
       </div>
+      {/* DESKTOP */}
+
+      {/* MOBILE */}
+      <div class="tablet:hidden w-full flex justify-end">
+        <button onClick={() => toggleMenu()} className="text-white focus:outline-none">
+          {isOpen ? (
+            <>
+              <VscChromeClose size="2rem"/>
+            </>
+          ) : (
+            <>
+          {/* <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+          </svg> */}
+          <HiMenuAlt3 size="2rem"/>
+            
+            </>
+          )}
+        </button>
+
+        <div className={`w-auto flex items-center ${isOpen ? "block" : "hidden"}`}>
+          {/* <ul className="text-white flex-row">
+            <li><a href="#home" className="block px-2 py-1">Home</a></li>
+            <li><a href="#about" className="block px-2 py-1">Sobre</a></li>
+            <li><a href="#services" className="block px-2 py-1">Projetos</a></li>
+            <li><a href="#contact" className="block px-2 py-1">Contact</a></li>
+          </ul> */}
+          <div className="flex-row">
+            <div className="block px-2 py-1">
+              <a href="#home" class="inline-flex items-center">
+                <AiOutlineHome />
+                Home
+              </a>
+            </div>
+
+            <div className="block px-2 py-1">
+              <a href="#sobre" class="inline-flex items-center">
+                <AiOutlineUser />
+                Sobre
+              </a>
+            </div>
+
+            <div className="block px-2 py-1">
+              <a href="#projetos" class="inline-flex items-center">
+                <AiOutlineFundProjectionScreen />
+                Projetos
+              </a>
+            </div>
+
+            <div className="flex justify-center">
+              <div className="inline-flex space-x-3">
+                <a
+                  className="hover:text-blue-500"
+                  href="https://github.com/rafaelBayer"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  title="Clique para acessar o Github"
+                >
+                  <AiFillGithub size="1.5rem" />
+                </a>
+
+                <a
+                  className=" hover:text-blue-500"
+                  href="https://www.linkedin.com/in/rafaelbayer0/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  title="Clique para acessar o Linkedin"
+                >
+                  <AiFillLinkedin size="1.5rem" />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* MOBILE */}
     </header>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens: {
+      mob: {'min': "375px", 'max': '767px'},
+      tablet: "768px",
+      laptop: "1024px",
+      desktop: "1280px",
+      laptopl: "1440px",
+    },
     extend: {},
   },
   plugins: [],
