0397d36e2a7b60a35e06f5f86ef8fddcc984e99c
Rafael Bayer <rafael.bayer@tegis.com.br>
2024-07-22 17:15:31 -0300

    Adicionado Tela home Adicionado Frase inicial e imagem de inicio do portifolio

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,22 +1,32 @@
-import React from 'react'
+import React from "react";
 
-import Template from './template/Template'
-import Imagem from '../assets/codeImage.png'
+import Template from "./template/Template";
+import Imagem from "../assets/codeImage.png";
 
 const Home = () => {
-    const page = "Home";
+  const page = "Home";
   return (
     <Template page={page}>
-      <div className="flex flex-col">
-        <div className="w-full text-left">
-          
+      <div id="home" className="text-white flex flex-col tablet:flex-row p-12">
+        <div className="w-full tablet:w-1/2 flex flex-col items-center justify-center">
+          {/* Desktop */}
+          <h1 className="mobile:hidden text-2xl tablet:text-4xl">Bem-vindo! Eu sou <strong className="text-blue-500">Rafael Bayer</strong></h1>
+          {/* Desktop */}
+
+          {/* Mobile */}
+          <h1 className="tablet:hidden text-2xl tablet:text-4xl">Bem-vindo!</h1>
+          <h1 className="tablet:hidden text-2xl tablet:text-4xl mb-1">Eu sou <strong className="text-blue-500">Rafael Bayer</strong></h1>
+          {/* Mobile */}
+
+          <p className="text-xl tablet:text-3xl">Desenvolvedor Full-stack</p>
+        </div>
+
+        <div className="w-full tablet:w-1/2">
+        <img src={Imagem} alt="a" />
         </div>
       </div>
-       
-
-       {/* <img src={Imagem} alt="a" /> */}
     </Template>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
