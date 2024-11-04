f012e10b9dad831bb24c7ec3fa790041e0ba63e4
Rafael Bayer <rafael.bayer@tegis.com.br>
2024-11-04 14:49:51 -0300

    Adicionado vercel analytics

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
 import Router from './routes/Router';
+import { Analytics } from "@vercel/analytics/react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router />
+    <Analytics />
   </React.StrictMode>
 );
 
