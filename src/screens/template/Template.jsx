e5a203b79fe8a8e6aa7be04bdf053c86c1b8ea09
rafael.bayer <rafael.bayer@tegis.com.br>
2024-11-22 23:19:07 -0300

    Adicionado react-helmet Ajustado head do template usando react-helmet para ajustar o SEO

--- src/screens/template/Template.jsx
+++ src/screens/template/Template.jsx
@@ -1,21 +1,32 @@
-import React, { useEffect } from "react";
+import React from "react";
+import { Helmet } from "react-helmet";
+
 import Header from "./Header";
 import Footer from "./Footer";
 
 const Template = ({ page, children }) => {
-  useEffect(() => {
-    document.title = page + " | Rafael Bayer";
-  }, [page]);
   return (
     <div className="overflow-x-hidden antialiased absolute top-0 z-[-2] h-screen  w-screen bg-gray-900">
+      <Helmet>
+        <title>{page} | Rafael Bayer</title>
+        <meta
+          name="description"
+          content={"Portfólio de Rafael Bayer, desenvolvedor Full Stack."}
+        />
+        <meta
+          name="keywords"
+          content={
+            "React.js, PHP, MySQL, APIs RESTful, Desenvolvimento Web, Full Stack"
+          }
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Helmet>
       <div className="container mx-auto">
         <Header />
         {children}
         <Footer />
       </div>
     </div>
-    // <div className="container mx-auto bg-gray-900">
-    // </div>
   );
 };
 
